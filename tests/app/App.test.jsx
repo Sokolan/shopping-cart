312b75c83e9ca638c09121d4404ef28b5f2f3967
Sokolan <valerysokolan@gmail.com>
2024-02-16 20:57:14 +0200

    added config for vercel deployment

--- tests/app/App.test.jsx
+++ tests/app/App.test.jsx
@@ -5,9 +5,5 @@ import App from "../../src/assets/components/app/App";
 describe("App", () => {
   it("renders headline", () => {
     render(<App title="React" />);
-
-    // screen.debug();
-
-    // check if App components renders headline
   });
 });
